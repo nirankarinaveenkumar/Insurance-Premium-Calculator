0862b51868940122a879d7c813a884ffedba38bd
Naveen Kumar Nirankari <61646488+nirankarinaveenkumar@users.noreply.github.com>
2020-06-27 12:34:02 -0500

    Update model.py

--- model.py
+++ model.py
@@ -37,9 +37,9 @@ lr_poly = LinearRegression()
 #X_test_poly = poly.transform(X_test_org)
 lr_poly.fit(X_train_org, y_train)
 
-listo=np.array([20, 0, 1, 10, 1, 0, 1, 0, 0, 0, 1]).reshape(1,-1)
+#listo=np.array([20, 0, 1, 10, 1, 0, 1, 0, 0, 0, 1]).reshape(1,-1)
 #print(listo.shape)
-print(lr_poly.predict(listo))
+#print(lr_poly.predict(listo))
 
 pickle.dump(lr_poly, open('model.pkl','wb'))
 
